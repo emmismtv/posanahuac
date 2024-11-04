10d3df994814175a54dd1be5824c52fe6843cf67
Omar RodVaz <bumblebeegamer88@gmail.com>
2024-11-04 09:23:46 -0600

    Clases
    Clases de usuario e inventario

---
+++ PuntoVentaApp/Models/Inventario.cs
@@ -0,0 +1,17 @@
+namespace PuntoVentaApp.Models;
+
+public class Inventario
+{
+    public string? Nombreproducto { get; set; }
+    public int? CantDisponible { get; set; }
+    public string? ID { get; set; }
+    
+
+    public Inventario
+    
+    public void Disponibilidad {}
+    
+    public void Agregar {}
+
+    public void Quitar{}
+} 
---
+++ PuntoVentaApp/Models/Usuario.cs
@@ -0,0 +1,15 @@
+namespace PuntoVentaApp.Models;
+
+public class Usuario
+{
+    public string? ID  { get; set; }
+    public string? NOMBRE  { get; set; }
+    public string? Correo  { get; set; }
+
+    public Usuario 
+
+    public HistorialCompras {}
+
+    
+    public CuponesDisponibles {}
+}
