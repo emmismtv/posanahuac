8e0f5be7ea31fc9279a416ecb558ea47f5144cfd
Omar RodVaz <bumblebeegamer88@gmail.com>
2024-11-04 09:24:35 -0600

    Merge

--- PuntoVentaApp/obj/Debug/net8.0/PuntoVentaApp.AssemblyInfo.cs
+++ PuntoVentaApp/obj/Debug/net8.0/PuntoVentaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuntoVentaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7e83b93b851c63beb74a3731b70c7d411cb7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d3df994814175a54dd1be5824c52fe6843cf67")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuntoVentaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuntoVentaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
